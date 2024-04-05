6d16a651f93b6c557a2442a44c3decf643351015
w1tel <aleksandrremnev774@gmail.com>
2024-04-05 19:03:49 +0300

    Start project

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
+GAME_TITLE = "Tic-Tac"
+
+
 def greeting():
-    pass
+    print(f"Welcome to {GAME_TITLE}")
 
 
-def start_game_menu():
-    print("Welcome to Tic-Tac")
-    choose = input('Choose a cross or a toe:')
+def get_player_move():
+    choose = input('Choose a X or 0:')
+    return choose
 
 
-def creat_field():
-    print("1 / 2 / 3", )
+def create_field():
+    board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    return board
+
+
+def show_field(board):
+    print(f"{board[0]} | {board[1]} | {board[2]}")
+    print(f"{board[3]} | {board[4]} | {board[5]}")
+    print(f"{board[6]} | {board[7]} | {board[8]}")
+
+
+def change_board(board, player_token, player_move):
+    return board
+
+
+def main():
+    greeting()
+    board = create_field()
+    show_field(board)
+    player_token = "X"
+    while True:
+        player_move = get_player_move()
+        board = change_board(board, player_token, player_move)
+        if player_token == "X":
+            player_token = "0"
+        elif player_token == "0":
+            player_token = "X"
+
+        show_field(board)
+
+
+if __name__ == "__main__":
+    main()

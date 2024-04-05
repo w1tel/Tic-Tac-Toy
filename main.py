66e795586b33517899f8f1cb046eaa03a6054f88
w1tel <aleksandrremnev774@gmail.com>
2024-04-05 18:14:57 +0300

    Start project

---
+++ main.py
@@ -0,0 +1,11 @@
+def greeting():
+    pass
+
+
+def start_game_menu():
+    print("Welcome to Tic-Tac")
+    choose = input('Choose a cross or a toe:')
+
+
+def creat_field():
+    print("1 / 2 / 3", )
